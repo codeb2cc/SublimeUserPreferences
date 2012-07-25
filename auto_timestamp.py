32a1cb6b663ca73dcaf9d2a02b5b440a466468c9
Codeb Fan <codeb2cc@gmail.com>
2012-07-26 02:19:11 +0800

    Update timestamp tags and preferences settings

--- auto_timestamp.py
+++ auto_timestamp.py
@@ -1,35 +1,34 @@
 # Description: Auto update file modification stamp
 # Author: Codeb Fan <codeb2cc@gmail.com>
-# Last Modified: 2012-04-13 20:08
+# Last Change: 2012-07-26 01:59
 #
 
 import datetime, re
 import sublime, sublime_plugin
 
 def _replace_stamp(edit, view, limit=10):
-	regexps = [
-		r'(.*)(@lastChange ).*$',
-		r'(.*)(Last Modified: ).*$',
-	]
+    regexps = [
+        r'(.*)(@lastChange ).*$',
+        r'(.*)(Last Change: ).*$',
+    ]
 
-	for regexp in regexps:
-		region = view.find(regexp, 0, sublime.IGNORECASE)
+    for regexp in regexps:
+        region = view.find(regexp, 0, sublime.IGNORECASE)
 
-		if region and view.rowcol(region.begin())[0] < limit:
-			dt = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
-			stamp = re.sub(regexp, r'\1\2', view.substr(region)) + dt
-			view.replace(edit, region, stamp)
-			break
-
+        if region and view.rowcol(region.begin())[0] < limit:
+            dt = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+            stamp = re.sub(regexp, r'\1\2', view.substr(region)) + dt
+            view.replace(edit, region, stamp)
+            break
 
 class AutoTimestampCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		_replace_stamp(edit, self.view)
+    def run(self, edit):
+        _replace_stamp(edit, self.view)
 
 class AutoTimestamp(sublime_plugin.EventListener):
-	def on_pre_save(self, view):
-		edit = view.begin_edit()
+    def on_pre_save(self, view):
+        edit = view.begin_edit()
 
-		_replace_stamp(edit, view)
+        _replace_stamp(edit, view)
 
-		view.end_edit(edit)
+        view.end_edit(edit)
